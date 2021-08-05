f7a5f15db85c16a9a425aeb3e62862e717b84fda
BlackOps-0xf <bundedavid98@gmail.com>
2021-08-05 20:13:53 +0100

    100-get_endianness.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,17 @@
 #include "holberton.h"
 
 /**
- * get_bit - returns the value of a bit at a given index.
- * @n: number to check bits in
- * @index: index at which to check bit
- *
- * Return: value of the bit, or -1 if there is an error
+ * get_bit - value of a bit at a given index
+ * @n: decimal parameter
+ * @index: index
+ * Return: val
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
+	int bit;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+	bit = (n >> index);
+	if (index > 32)
+	return (-1);
+	return (bit & 1);
 }
